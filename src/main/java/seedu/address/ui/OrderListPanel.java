212b77283433641b3bf6be75fe87c5a7d876f0d4
chen_haozhe <chenzmxy@gmail.com>
2026-03-17 11:06:33 +0800

    Create windows for OrderListPanel

---
+++ src/main/java/seedu/address/ui/OrderListPanel.java
@@ -0,0 +1,56 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.order.Order;
+import seedu.address.model.person.Person;
+
+/**
+ * Panel containing the list of orders.
+ */
+public class OrderListPanel extends UiPart<Region> {
+    private static final String FXML = "OrderListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(OrderListPanel.class);
+
+    @FXML
+    private ListView<Order> orderListView;
+
+    /**
+     * Creates an {@code OrderListPanel} with the given {@code ObservableList} of orders and persons.
+     */
+    public OrderListPanel(ObservableList<Order> orderList, ObservableList<Person> personList) {
+        super(FXML);
+        orderListView.setItems(orderList);
+        orderListView.setCellFactory(listView -> new OrderListViewCell(personList));
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of an {@code Order} using an {@code OrderCard}.
+     */
+    class OrderListViewCell extends ListCell<Order> {
+        private final ObservableList<Person> personList;
+
+        OrderListViewCell(ObservableList<Person> personList) {
+            this.personList = personList;
+        }
+
+        @Override
+        protected void updateItem(Order order, boolean empty) {
+            super.updateItem(order, empty);
+
+            if (empty || order == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new OrderCard(order, getIndex() + 1, personList).getRoot());
+            }
+        }
+    }
+
+}

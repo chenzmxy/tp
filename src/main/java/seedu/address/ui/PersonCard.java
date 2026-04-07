012bd4101948595e81abff1a3f6f73acff5155b8
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2026-04-07 19:30:13 +0800

    Remove unused WRAP_LABEL_PADDING constant

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -21,8 +21,7 @@ import seedu.address.model.person.Person;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
-    private static final double WRAP_LABEL_PADDING = 40;
-
+    
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords

623d16a0c0dca69485e6b15521b951f324c18e43
Nathan0820 <nathanwyj0629@gmail.com>
2026-04-11 14:38:18 +0800

    Add warning message when quantity is changed

--- src/main/java/seedu/address/logic/commands/EditOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/EditOrderCommand.java
@@ -51,6 +51,9 @@ public class EditOrderCommand extends Command {
     public static final String MESSAGE_EDIT_ORDER_SUCCESS = "Edited Order: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_UNKNOWN_CUSTOMER = "The customer for this order is unknown.";
+    public static final String MESSAGE_QUANTITY_CHANGED_WARNING = "⚠\uFE0F WARNING: The quantity has been changed to %1$s. "
+            + "Are you sure the status is still %2$s? If not, please use the command 'edit-o %3$d s/[NEW_STATUS]' "
+            + "to update the status.\n";
 
     private final Index index;
     private final EditOrderDescriptor editOrderDescriptor;
@@ -95,6 +98,16 @@ public class EditOrderCommand extends Command {
         if (editOrderDescriptor.getDeliveryTime().isPresent()
                 && !editedOrder.getDeliveryTime().isInFuture()) {
             resultMessage += AddOrderCommand.MESSAGE_PAST_TIME + "\n";
+        }
+
+        if (editOrderDescriptor.getQuantity().isPresent()
+                && !orderToEdit.getQuantity().equals(editOrderDescriptor.getQuantity().get())) {
+            resultMessage += String.format(
+                MESSAGE_QUANTITY_CHANGED_WARNING,
+                editOrderDescriptor.getQuantity().get().value,
+                editedOrder.getStatus(),
+                index.getOneBased()
+            );
         }
         resultMessage += String.format(
             MESSAGE_EDIT_ORDER_SUCCESS,
